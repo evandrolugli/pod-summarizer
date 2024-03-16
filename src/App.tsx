0d6320836ba9238ce06065513a389a6cb49fe2dc
Evandro Lugli <evandro.lugli@gmail.com>
2024-03-16 21:40:51 +1100

    Update header

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">Podcast AI</h1>
         <div className="flex items-center gap-3">
-          <span>Develop with 💜</span>
+          <span className="text-sm text-zinc-400">Developed with 💜</span>
           <Button>Github</Button>
         </div>
       </div>
