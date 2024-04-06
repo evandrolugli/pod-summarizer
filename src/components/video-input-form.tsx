b74e8cb4a37e25716e43b1bf4342a038273f2344
Evandro Lugli <evandro.lugli@gmail.com>
2024-04-06 19:28:23 +1100

    Fix: vercel build - variable

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -66,7 +66,7 @@ export function VideoInputForm (){
     async function handleUploadVideo(event: FormEvent<HTMLFormElement>){
         event.preventDefault()
 
-        const prompt = promptInputRef.current?.value
+        // const prompt = promptInputRef.current?.value
 
         if (!videoFile) {
             return
