3cd19592ee3b672055293f501dfbc72422f4a148
Evandro Lugli <evandro.lugli@gmail.com>
2024-06-06 17:03:15 +1000

    Fix transcription

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,6 +6,7 @@ import { Button } from './ui/button';
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react';
 import { getFFmpeg } from '@/lib/ffmpeg';
 import { fetchFile } from '@ffmpeg/util';
+import { api } from '@/lib/axios';
 
 export function VideoInputForm (){
     const [videoFile, setVideoFile] = useState<File | null>(null)
@@ -75,7 +76,23 @@ export function VideoInputForm (){
 
         const audioFile = await convertVideoToAudio(videoFile)
 
-        console.log(audioFile, prompt)
+        //console.log(audioFile, prompt)
+
+        const data = new FormData()
+
+        data.append('file', audioFile)
+
+        const response = await api.post('/videos', data)
+
+        //console.log(response.data)
+
+        const videoId = response.data.video.id
+
+        await api.post(`/videos/${videoId}/transcription`, {
+          prompt, 
+        })
+
+        console.log('finished')
     }
 
     const previewURL = useMemo(() => {
